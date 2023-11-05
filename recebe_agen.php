eba4f98e74dd644b3d48b128a897fb11656c3a4b
Damasceno3301 <71456387+Damascenopog@users.noreply.github.com>
2023-11-04 23:00:25 -0300

    Bug no operacao e com sql funcionando

--- recebe_agen.php
+++ recebe_agen.php
@@ -5,12 +5,27 @@ include 'conecta_mysqli.inc';
 $operacao = $_POST["operacao"];
 $erros = [];
 
-if ($operacao == "cadastrar") {
-    $orario_inicio = $_POST["horario_inicio"];
-    $data_agenda = $_POST["data_agenda"];
-}
+if ($operacao == "agendar") {
+    if (empty($_POST["id_funcionario"]) || empty($_POST["id_servico"]) || empty($_POST["horario_inicio"]) || empty($_POST["data_agenda"])) {
+        $erros[] = "Por favor, preencha todos os campos obrigatórios.";
+    } 
+    else {
+        $id_funcionario = $_POST["id_funcionario"];
+        $id_servico = $_POST["id_servico"];
+        $horario_inicio = $_POST["horario_inicio"];
+        $data_agenda = $_POST["data_agenda"];
 
-$sql = "INSERT INTO usuarios (horario_inicio, data_agenda) VALUES ('$horario_inicio', '$data_agenda')";
+        $stmt = $mysqli->prepare("INSERT INTO usuarios (id_funcionario, id_servico, horario_inicio, data_agenda) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("iiss", $id_funcionario, $id_servico, $horario_inicio, $data_agenda);
+
+        if ($stmt->execute()) {
+            $mensagem = "Agendamento realizado com sucesso!";
+        } else {
+            $erros[] = "Ocorreu um erro ao realizar o agendamento. Por favor, tente novamente.";
+        }
+        $stmt->close();
+    }
+}
 
 mysqli_close($mysqli);
 ?>
