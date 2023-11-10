523ae28d6daed05b5dced12ae8f19bfd56599b2f
LCTRAIN <142441068+LCTRAIN@users.noreply.github.com>
2023-11-09 23:40:12 -0300

    login para adm (nao completo)

--- login.php
+++ login.php
@@ -1,35 +1,57 @@
-<?php 
+<?php
 include "conecta_mysqli.inc";
 session_start();
 
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
+// Consulta a tabela de administradores
+$sql = "SELECT * FROM administradores WHERE email = '$email';";
+$res = mysqli_query($mysqli, $sql);
+
+// Se o email estiver na tabela de administradores
+if (mysqli_num_rows($res) == 1) {
+    $administrador = mysqli_fetch_array($res);
+
+    // Verifica se a senha está correta para o administrador
+    if (password_verify(trim($senha), $administrador["senha"])) {
+        // Usuário autenticado como administrador
+        $_SESSION["email"] = $email;
+        $_SESSION["senha"] = $senha;
+        header("Location: adm_visu.php");
+        exit;
+    } else {
+        $_SESSION['senha_invalida'] = "Senha incorreta!";
+        header("Location: form_login.php");
+        exit;
+    }
+}
+
+// Se o email não pertencer a um administrador, consulte a tabela de usuários
 $sql = "SELECT * FROM usuarios WHERE email = '$email';";
 $res = mysqli_query($mysqli, $sql);
 
-/* Testa se não encontrou o email */
+// Testa se não encontrou o email
 if (mysqli_num_rows($res) != 1) {
     $_SESSION['erro_login'] = "E-mail não encontrado!";
     header("Location: form_login.php");
+    exit;
 } else {
     $usuario = mysqli_fetch_array($res);
 
-    /* Testa se a senha está errada */
+    // Verifica se a senha está errada para o usuário comum
     if (!password_verify($senha, $usuario["senha"])) {
-        $_SESSION['senha_invalida'] = "Senha inválida!";
+        $_SESSION['senha_invalida'] = "Senha incorreta!";
         header("Location: form_login.php");
+        exit;
     } else {
-        session_start();
         $_SESSION["email"] = $email;
-        $_SESSION["senha"] = $usuario["senha"];
-        // direciona para a página inicial
-
-        header("Location: form_cadastro.php");
-
+        $_SESSION["senha"] = $senha;
+        // Direciona para a página inicial
         header("Location: agendamento.php");
-
+        exit;
     }
 }
+
 mysqli_close($mysqli);
 ?>
