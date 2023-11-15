8d4b4c13a5a8dc22d6891f6685bdd5db0c418e78
LCTRAIN <142441068+LCTRAIN@users.noreply.github.com>
2023-11-15 10:19:19 -0300

    alterar dnv

--- pagina_cliente.php
+++ pagina_cliente.php
@@ -3,7 +3,6 @@ session_start();
 // Verifica se o usuário está logado
 if (isset($_SESSION["email"])) {
     $emailUsuario = $_SESSION["email"]; // Obtém o email do usuário da sessão
-    echo "Usuário logado: $emailUsuario";
     // Conectar ao banco de dados (substitua os detalhes com os seus)
     $conexao = new mysqli("localhost", "barbeariajulius", "123", "BARBEARIAJULIUS");
 
@@ -13,7 +12,7 @@ if (isset($_SESSION["email"])) {
     }
 
     // Consulta para obter o ID do usuário com base no email
-    $query = "SELECT id_usuario FROM usuarios WHERE email = ?";
+    $query = "SELECT id_usuario, nome, email FROM usuarios WHERE email = ?";
     $stmt = $conexao->prepare($query);
     $stmt->bind_param("s", $emailUsuario);
     $stmt->execute();
@@ -23,22 +22,27 @@ if (isset($_SESSION["email"])) {
     if ($resultado->num_rows > 0) {
         $row = $resultado->fetch_assoc();
         $id_usuario = $row['id_usuario'];
+        $nomeUsuario = $row['nome'];
+        $emailUsuario = $row['email'];
     } else {
-        header("Location: logado.html");
+        header("Location: pagina_cliente.php");
         exit;
     }
 
     // ... restante do código para atualizar os dados
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Recupera os dados do formulário
-        $novo_nome = $_POST["novo_nome"];
-        $novo_email = $_POST["novo_email"];
+        $novo_nome = htmlspecialchars($_POST["novo_nome"]);
+        $novo_email = htmlspecialchars($_POST["novo_email"]);
 
         // Preparar e executar a consulta SQL usando prepared statements
         $query_update = $conexao->prepare("UPDATE usuarios SET nome=?, email=? WHERE id_usuario=?");
         $query_update->bind_param("ssi", $novo_nome, $novo_email, $id_usuario);
 
         if ($query_update->execute()) {
+            // Atualiza os dados na sessão para refletir as mudanças
+            $_SESSION['email'] = $novo_email;
+
             // Redireciona de volta para a página de perfil após a alteração
             header("Location: pagina_cliente.php");
             exit();
@@ -55,6 +59,7 @@ if (isset($_SESSION["email"])) {
     exit;
 }
 ?>
+
 
 
 <!-- ... (restante do código HTML) -->
@@ -92,12 +97,13 @@ if (isset($_SESSION["email"])) {
                 <div class="dados">
                     <div class="informacao">
                         <h3>Nome:</h3>
-                        <p>Nome do Cliente</p>
+                        <p><?php echo htmlspecialchars($nomeUsuario); ?></p>
                     </div>
                     <div class="informacao">
                         <h3>Email:</h3>
-                        <p>cliente@email.com</p>
+                        <p><?php echo htmlspecialchars($emailUsuario); ?></p>
                     </div>
+
                 </div>
                 <button class="alterar-dados-btn">Alterar Dados</button>
             </div>
@@ -113,10 +119,10 @@ if (isset($_SESSION["email"])) {
                 <input type="hidden" id="id_usuario" name="id_usuario" value="<?php echo $id_usuario; ?>">   
                 
                 <label for="novo_nome">Novo Nome:</label>
-                <input type="text" id="novo_nome" name="novo_nome" required>
+                <input type="text" id="novo_nome" name="novo_nome" value="<?php echo htmlspecialchars($nomeUsuario); ?>" required>
                 
                 <label for="novo_email">Novo Email:</label>
-                <input type="email" id="novo_email" name="novo_email" required>
+                <input type="email" id="novo_email" name="novo_email" value="<?php echo htmlspecialchars($emailUsuario); ?>" required>
                 
                 <button type="submit" class="salvar-alteracoes-btn">Salvar Alterações</button>
             </form>
