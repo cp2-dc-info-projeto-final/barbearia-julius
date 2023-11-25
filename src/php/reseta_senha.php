9373994b47665bf74fb21dbfc04c175d7255d9a6
Lucas Melo <lucasmeloura@gmail.com>
2023-11-25 02:09:51 -0300

    esqueci senha

--- src/php/reseta_senha.php
+++ src/php/reseta_senha.php
@@ -1,16 +1,23 @@
 <!-- formulario.php -->
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
+<html lang="pt-BR">
     <meta charset="UTF-8">
-    <title>Redefinir Senha</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reseta_senha.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
+    <title>Seus Dados - Barbearia Julius</title>
 </head>
 <body>
-    <form action="reseta.php" method="post">
+    <section>
+        <div class="circle"></div>
+        <header>
+            <a href="#"><img src="../img/logo.png" alt="" class="logo"></a>
+            <form action="reseta.php" method="post">
         <label for="email">E-mail:</label>
         <input type="email" id="email" name="email" required>
         <button type="submit">Redefinir Senha</button>
     </form>
-</body>
 </html>
 
