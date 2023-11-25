43b864040066bd0594914fe84f4514c3dd02aeaa
LCTRAIN <142441068+LCTRAIN@users.noreply.github.com>
2023-11-25 02:10:47 -0300

    erros não resolvidos no agendamento

--- src/js/agendamento.js
+++ src/js/agendamento.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
     $('#id_funcionario').on('change', function() {
-        const selecionado = $(this).val().toString(); // Certifique-se de que é uma string
+        const selecionado = $(this).val().toString();
+        const dataSelecionada = $('#data_agenda').val(); // Obtém a data selecionada no formato dd-mm-aaaa
+
+        // Converte a data para o formato aaaa-mm-dd
+        const partesData = dataSelecionada.split('-');
+        const dataFormatoCorreto = partesData[2] + '-' + partesData[1] + '-' + partesData[0];
+
         $('#horario_inicio').empty();
 
         if (selecionado !== '0') {
             $.ajax({
                 url: 'consultar_horarios.php',
                 type: 'POST',
-                data: { id_funcionario: selecionado },
+                data: {
+                    id_funcionario: selecionado,
+                    data_agenda: dataFormatoCorreto // Envia a data no formato aaaa-mm-dd para o servidor
+                },
                 dataType: 'json',
                 success: function(response) {
-                    $.each(response, function(index, horario) {
+                    // Limpa os horários disponíveis e adiciona os novos horários
+                    response.forEach(function(horario) {
                         $('#horario_inicio').append($('<option>', {
                             value: horario,
                             text: horario
--- src/php/consultar_horarios.php
+++ src/php/consultar_horarios.php
@@ -2,27 +2,48 @@
 include '../inc/conecta_mysqli.inc';
 
 $id_funcionario = $_POST['id_funcionario'];
+$data_agenda = $_POST['data_agenda'];
 
-// Consultar os horários já agendados
-$query_agendados = "SELECT DISTINCT horario_inicio FROM agendamento WHERE id_funcionario = ?";
+// Consultar os horários já agendados para o funcionário e a data especificados
+$query_agendados = "SELECT horario_inicio FROM agendamento WHERE id_funcionario = ? AND data_agenda = ?";
 $stmt_agendados = $mysqli->prepare($query_agendados);
-$stmt_agendados->bind_param("i", $id_funcionario);
+$stmt_agendados->bind_param("is", $id_funcionario, $data_agenda);
 $stmt_agendados->execute();
 $stmt_agendados->bind_result($horario_agendado);
 
-$horarios_agendados = array();
+$horarios_ocupados = array();
 
 while ($stmt_agendados->fetch()) {
-    $horarios_agendados[] = $horario_agendado;
+    $horarios_ocupados[] = $horario_agendado;
 }
 
 $stmt_agendados->close();
 
-// Consultar todos os horários disponíveis
+// Todos os horários possíveis
 $horarios_disponiveis = ['09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
 
-// Remover os horários agendados da lista de horários disponíveis
-$horarios_disponiveis = array_diff($horarios_disponiveis, $horarios_agendados);
+// Verificar os horários ocupados antes da remoção
+echo "Horários ocupados antes da remoção: ";
+var_dump($horarios_ocupados);
+
+// Verificar os horários disponíveis antes da remoção
+echo "Horários disponíveis antes da remoção: ";
+var_dump($horarios_disponiveis);
+
+// Remover os horários já agendados para o funcionário na data especificada da lista de horários disponíveis
+foreach ($horarios_ocupados as $horario_ocupado) {
+    $key = array_search($horario_ocupado, $horarios_disponiveis);
+    if ($key !== false) {
+        unset($horarios_disponiveis[$key]);
+    }
+}
+
+// Reindexar o array após a remoção
+$horarios_disponiveis = array_values($horarios_disponiveis);
+
+// Verificar os horários disponíveis após a remoção
+echo "Horários disponíveis após a remoção: ";
+var_dump($horarios_disponiveis);
 
 echo json_encode(array_values($horarios_disponiveis));
 
