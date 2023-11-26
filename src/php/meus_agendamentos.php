c4765b873991a3c08a9e2b1ddc44c153fa5f7c2b
LCTRAIN <142441068+LCTRAIN@users.noreply.github.com>
2023-11-26 01:06:11 -0300

    meus agendamentos, usuarios.

--- src/php/meus_agendamentos.php
+++ src/php/meus_agendamentos.php
@@ -5,10 +5,13 @@ include '../inc/conecta_mysqli.inc';
 if (isset($_SESSION["email"])) {
     $emailUsuario = $_SESSION["email"];
 
-    $query = "SELECT agendamento.data_agenda, agendamento.horario_inicio, usuarios.nome AS nome_usuario, usuarios.email AS email_usuario, funcionarios.nome AS nome_funcionario
+    $query = "SELECT agendamento.id_agendamento, agendamento.data_agenda, agendamento.horario_inicio, 
+              usuarios.nome AS nome_usuario, usuarios.email AS email_usuario, 
+              funcionarios.nome AS nome_funcionario, servico.descricao AS descricao_servico
               FROM agendamento
               INNER JOIN usuarios ON agendamento.id_usuario = usuarios.id_usuario
               INNER JOIN funcionarios ON agendamento.id_funcionario = funcionarios.id_funcionario
+              INNER JOIN servico ON agendamento.id_servico = servico.id_servico
               WHERE usuarios.email = ?";
     $stmt = $mysqli->prepare($query);
     $stmt->bind_param("s", $emailUsuario);
@@ -18,7 +21,7 @@ if (isset($_SESSION["email"])) {
     if ($result->num_rows > 0) {
         echo "<h3>Agendamentos:</h3>";
         echo "<table border='1'>";
-        echo "<tr><th>Nome</th><th>Email</th><th>Funcionário</th><th>Data</th><th>Hora</th></tr>";
+        echo "<tr><th>Nome</th><th>Email</th><th>Funcionário</th><th>Data</th><th>Hora</th><th>Serviço</th><th>Numero do Agendamento</th></tr>";
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row['nome_usuario'] . "</td>";
@@ -26,6 +29,8 @@ if (isset($_SESSION["email"])) {
             echo "<td>" . $row['nome_funcionario'] . "</td>";
             echo "<td>" . $row['data_agenda'] . "</td>";
             echo "<td>" . $row['horario_inicio'] . "</td>";
+            echo "<td>" . $row['descricao_servico'] . "</td>"; // Adicionando a coluna do serviço
+            echo "<td>" . $row['id_agendamento'] . "</td>";
             echo "</tr>";
         }
         echo "</table>";
