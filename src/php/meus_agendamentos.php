b9cd8752b9d7eaedde76b0fa9b5ac0f80d6d214d
LCTRAIN <142441068+LCTRAIN@users.noreply.github.com>
2023-11-26 01:13:13 -0300

    cancelar agendamento

--- src/php/meus_agendamentos.php
+++ src/php/meus_agendamentos.php
@@ -21,7 +21,7 @@ if (isset($_SESSION["email"])) {
     if ($result->num_rows > 0) {
         echo "<h3>Agendamentos:</h3>";
         echo "<table border='1'>";
-        echo "<tr><th>Nome</th><th>Email</th><th>Funcionário</th><th>Data</th><th>Hora</th><th>Serviço</th><th>Numero do Agendamento</th></tr>";
+        echo "<tr><th>Nome</th><th>Email</th><th>Funcionário</th><th>Data</th><th>Hora</th><th>Serviço</th><th>Numero do Agendamento</th><th>Cancelar</th></tr>";
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row['nome_usuario'] . "</td>";
@@ -29,11 +29,31 @@ if (isset($_SESSION["email"])) {
             echo "<td>" . $row['nome_funcionario'] . "</td>";
             echo "<td>" . $row['data_agenda'] . "</td>";
             echo "<td>" . $row['horario_inicio'] . "</td>";
-            echo "<td>" . $row['descricao_servico'] . "</td>"; // Adicionando a coluna do serviço
+            echo "<td>" . $row['descricao_servico'] . "</td>";
             echo "<td>" . $row['id_agendamento'] . "</td>";
+            echo "<td><form method='post'><button type='submit' name='cancelar' value='" . $row['id_agendamento'] . "'>Cancelar</button></form></td>";
             echo "</tr>";
         }
         echo "</table>";
+
+        // Verifica se o botão de cancelar foi pressionado
+        if (isset($_POST['cancelar'])) {
+            $id_agendamento_cancelar = $_POST['cancelar'];
+
+            // Query para deletar o agendamento
+            $query_delete = "DELETE FROM agendamento WHERE id_agendamento = ?";
+            $stmt_delete = $mysqli->prepare($query_delete);
+            $stmt_delete->bind_param("i", $id_agendamento_cancelar);
+            $stmt_delete->execute();
+
+            // Verifica se a deleção foi bem-sucedida
+            if ($stmt_delete->affected_rows > 0) {
+                echo "Agendamento cancelado com sucesso!";
+                // Aqui você pode adicionar redirecionamento ou outras ações após o cancelamento
+            } else {
+                echo "Erro ao cancelar o agendamento.";
+            }
+        }
     } else {
         echo "Nenhum agendamento encontrado para este usuário.";
     }
