60cdcf12f00841d62b96e7f77fd32405ad6560ff
LCTRAIN <142441068+LCTRAIN@users.noreply.github.com>
2023-11-26 19:47:24 -0300

    cadastro verifica nas tabelas

--- src/php/recebe_dados.php
+++ src/php/recebe_dados.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 include 'enviar.php';
@@ -16,11 +17,19 @@ if ($operacao == "cadastrar") {
     $email = $_POST["email"];
 
     // Verificar se o e-mail já existe na tabela de usuários
-    $verifica_email = "SELECT * FROM usuarios WHERE email = '$email'";
-    $resultado = mysqli_query($mysqli, $verifica_email);
+    $verifica_email_usuario = "SELECT * FROM usuarios WHERE email = '$email'";
+    $resultado_usuario = mysqli_query($mysqli, $verifica_email_usuario);
 
-    if (mysqli_num_rows($resultado) > 0) {
-        $erros[] = "E-mail já cadastrado. Por favor, escolha outro e-mail.";
+    // Verificar se o e-mail já existe na tabela de funcionarios
+    $verifica_email_funcionario = "SELECT * FROM funcionarios WHERE email = '$email'";
+    $resultado_funcionario = mysqli_query($mysqli, $verifica_email_funcionario);
+
+    // Verificar se o e-mail já existe na tabela de administradores
+    $verifica_email_administrador = "SELECT * FROM administradores WHERE email = '$email'";
+    $resultado_administrador = mysqli_query($mysqli, $verifica_email_administrador);
+
+    if (mysqli_num_rows($resultado_usuario) > 0 || mysqli_num_rows($resultado_funcionario) > 0 || mysqli_num_rows($resultado_administrador) > 0) {
+        $erros[] = "E-mail já cadastrado ou similar a um funcionário ou administrador. Por favor, escolha outro e-mail.";
     }
 
     if (empty($senha) || strlen($senha) < 6) {
@@ -36,9 +45,8 @@ if ($operacao == "cadastrar") {
         header("Location: form_cadastro.php");
         exit();
     }
-     
 
-// Armazene os dados do usuário temporariamente até a confirmação do email
+    // Armazene os dados do usuário temporariamente até a confirmação do email
     $_SESSION['temp_user_data'] = [
         'senha' => password_hash($senha, PASSWORD_DEFAULT),
         'nome' => $nome,
@@ -47,51 +55,49 @@ if ($operacao == "cadastrar") {
 
     $senha_cript = $_SESSION['temp_user_data']['senha'];
 
-    
     $sql = "INSERT INTO usuarios (senha, nome, email) VALUES ('$senha_cript', '$nome', '$email')";
 
+    if (mysqli_query($mysqli, $sql)) {
+        // Envio de email de confirmação
 
-        if (mysqli_query($mysqli, $sql)) {
-            // Envio de email de confirmação
 
+    $mail = new PHPMailer(true);
 
-        $mail = new PHPMailer(true);
+    try {
 
-        try {
+        //Configurações do servidor
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                   //Habilita a saída de debug (para fase de testes)
+        $mail->isSMTP();                                            //Define o envio por meio do SMTP
+        $mail->Host       = 'smtp.gmail.com';                       //Define o servidor SMTP utilizado para o envio
+        $mail->SMTPAuth   = true;                                   //Habilita a autenticação do SMTP
+        $mail->Username   = 'lp3.turma2023@gmail.com';               //usuário SMTP
+        $mail->Password   = 'tpqlektljcpmnfga';                      //senha SMTP
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Habilita a encriptação implícita TLS
+        $mail->Port       = 587;                                    //Porta TCP de conexão; use 587 se você tiver configurado `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-            //Configurações do servidor
-            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                   //Habilita a saída de debug (para fase de testes)
-            $mail->isSMTP();                                            //Define o envio por meio do SMTP
-            $mail->Host       = 'smtp.gmail.com';                       //Define o servidor SMTP utilizado para o envio
-            $mail->SMTPAuth   = true;                                   //Habilita a autenticação do SMTP
-            $mail->Username   = 'lp3.turma2023@gmail.com';               //usuário SMTP
-            $mail->Password   = 'tpqlektljcpmnfga';                      //senha SMTP
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Habilita a encriptação implícita TLS
-            $mail->Port       = 587;                                    //Porta TCP de conexão; use 587 se você tiver configurado `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        // Remetente e destinatário
+        $mail->setFrom('lp3.turma2023@gmail.com', 'Site do Professor');
+        $mail->addAddress($email, $nome);
 
-            // Remetente e destinatário
-            $mail->setFrom('lp3.turma2023@gmail.com', 'Site do Professor');
-            $mail->addAddress($email, $nome);
+        // Assunto do email
+        $assunto = 'Confirme seu email';
 
-            // Assunto do email
-            $assunto = 'Confirme seu email';
+        // Conteúdo do email
+        $mail->isHTML(true);
+        $mail->Subject = $assunto;
+        $link_confirmacao = "$codigo"; // Substitua pelo seu domínio
+        $mail->Body = 'Olá ' . $nome .'Seu email foi confirmado.' . '">Confirmar cadastro</a>';
 
-            // Conteúdo do email
-            $mail->isHTML(true);
-            $mail->Subject = $assunto;
-            $link_confirmacao = "$codigo"; // Substitua pelo seu domínio
-            $mail->Body = 'Olá ' . $nome .'Seu email foi confirmado.' . '">Confirmar cadastro</a>';
-
-            $mail->send();
-            
-            // Redirecionamento após o envio do email
-            header("Location: form_login.php");
-            exit();
-        } catch (Exception $e) {
-            $_SESSION['cadastro_erros'] = ["Erro ao enviar o email de confirmação: " . $e->getMessage()];
-            header("Location: form_cadastro.php");
-            exit();
-        }
+        $mail->send();
+        
+        // Redirecionamento após o envio do email
+        header("Location: form_login.php");
+        exit();
+    } catch (Exception $e) {
+        $_SESSION['cadastro_erros'] = ["Erro ao enviar o email de confirmação: " . $e->getMessage()];
+        header("Location: form_cadastro.php");
+        exit();
+    }
     } else {
         $_SESSION['cadastro_erros'] = ["Erro ao cadastrar usuário: " . mysqli_error($mysqli)];
         header("Location: form_cadastro.php");
