5a4d419dd44f744cf3af63a4c9627aca40a32117
anacasroline <95311688+anacasroline@users.noreply.github.com>
2023-11-29 16:44:59 -0300

    Add files via upload

--- src/php/processa_novo_funcionario.php
+++ src/php/processa_novo_funcionario.php
@@ -7,12 +7,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $numero = $_POST['numero'];
     $email = $_POST['email'];
 
-    $sql = "INSERT INTO funcionarios (nome, senha, numero, email) VALUES ('$nome', '$senha', '$numero', '$email')";
-
+    $senha_cript = password_hash($senha, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO funcionarios (nome, senha, numero, email)";
+    $sql .= "VALUES ('$nome','$senha_cript', $numero,'$email')";
+   
     if ($mysqli->query($sql) === TRUE) {
         header("Location: adm_visu.php?success=1"); // Redireciona com o parâmetro de sucesso
         exit;
-    } 
+    }
+    else{
+        echo ($mysqli->error);
+    }
 }else {
         header("Location: adm_visu.php?success=0"); // Redireciona com o parâmetro de falha
         exit;
